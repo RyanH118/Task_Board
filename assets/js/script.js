b8f888c23d3ed0575bb0a6fa323ccfab299325c9
ryanh <ryanhinson118@mail.com>
2024-04-11 18:55:00 -0400

    Added a function to generate unique task id.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,11 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    return nextId++;
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
 
-}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -18,12 +16,12 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+function handleAddTask(event) {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+function handleDeleteTask(event) {
 
 }
 
@@ -34,12 +32,10 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+    
+    $("#task-date").datepicker({
+        changeMonth: true,
+        changeYear: true
+    });
 
 });
-
-$( function() {
-    $( "#task-date" ).datepicker({
-      changeMonth: true,
-      changeYear: true
-    });
-  } );
